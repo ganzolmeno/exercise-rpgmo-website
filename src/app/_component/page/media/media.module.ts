3c24d72b3ef481d5c9a5005935b57f63e3f31caa
ganzolmeno <player220250@gmail.com>
2022-10-23 17:10:48 +0800

    npm i ng-gallery @angular/cdk

--- src/app/_component/page/media/media.module.ts
+++ src/app/_component/page/media/media.module.ts
@@ -2,12 +2,14 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MediaComponent } from './media.component';
+import { ScreenshotsBoxComponent } from './screenshots-box/screenshots-box.component';
 
 
 
 @NgModule({
   declarations: [
-    MediaComponent
+    MediaComponent,
+    ScreenshotsBoxComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/_component/page/media/screenshots-box/screenshots-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screenshots-box',
+  templateUrl: './screenshots-box.component.html',
+  styleUrls: ['./screenshots-box.component.scss']
+})
+export class ScreenshotsBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
