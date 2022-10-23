da0eac0d287f46efc0ada336d6dc36476bda2cba
ganzolmeno <player220250@gmail.com>
2022-10-23 21:54:08 +0800

    highscore init and console url

--- src/app/_component/page/highscore/highscore.module.ts
+++ src/app/_component/page/highscore/highscore.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighscoreComponent } from './highscore.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { HighscoreComponent } from './highscore.component';
     HighscoreComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{path: '**',component: HighscoreComponent}])
   ]
 })
 export class HighscoreModule { }
